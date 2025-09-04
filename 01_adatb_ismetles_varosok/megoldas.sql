b68c0f1f58b43d1dc565b75ff27edd176596b7c8
Aniko <nahajia@gmail.com>
2025-09-04 14:27:31 +0200

    01 adatb alap,25 feladat teljes

--- 01_adatb_ismetles_varosok/megoldas.sql
+++ 01_adatb_ismetles_varosok/megoldas.sql
@@ -114,3 +114,125 @@ GROUP BY megye.mnev;
 találat 20
 
 13.
+SELECT varostipus.vtip,COUNT(varos.id)
+FROM varostipus
+INNER JOIN varos 
+ON varos.vtipid=varostipus.id
+GROUP BY varostipus.vtip;
+5 darab
+14 
+SELECT varos.vnev, varos.nepesseg,varos.terulet
+FROM varos
+INNER JOIN megye
+ON varos.megyeid=megye.id
+WHERE megye.mnev="Vas"  
+ORDER BY `varos`.`nepesseg` DESC  
+LIMIT 1;
+Szombathely
+Másik megoldás:
+SELECT varos.vnev, varos.nepesseg,varos.terulet
+FROM varos
+INNER JOIN megye
+ON varos.megyeid=megye.id
+WHERE megye.mnev="Vas"  
+ORDER BY `varos`.`nepesseg` DESC  
+LIMIT 1;
+15.
+SELECT varos.vnev,varos.nepesseg
+from varos
+where varos.nepesseg>(select avg(varos.nepesseg)
+from varos)  
+ORDER BY `varos`.`nepesseg` ASC
+56 találat
+16.
+select varos.vnev,varos.nepesseg 
+from varos
+where varos.nepesseg<(select varos.nepesseg
+from varos
+inner join megye
+on varos.megyeid=megye.id
+where megye.mnev="Hajdú-Bihar"  
+ORDER BY `varos`.`nepesseg` ASC 
+limit 1);
+54 találat
+
+17.
+SELECT varos.vnev, varos.nepesseg, megye.mnev
+FROM varos 
+INNER JOIN megye 
+ON varos.megyeid = megye.id
+WHERE megye.mnev IN (select megye.mnev 
+FROM megye
+INNER JOIN varos
+ON megye.id = varos.megyeid
+WHERE varos.vnev LIKE "%város%");
+
+67 találat
+
+18.
+SELECT megye.mnev, varos.vnev
+FROM varos
+INNER JOIN megye
+ON varos.megyeid = megye.id
+WHERE varos.megyeid NOT IN (SELECT varos.megyeid
+FROM varos
+WHERE varos.vnev LIKE "%falu%")
+AND varos.nepesseg BETWEEN 10000 AND 20000;
+
+76 találat
+
+19. 
+INSERT INTO varos VALUES (NULL, "Kukutyin", 5, 10, "Hajdú járás", "Hajdú kistérség", 1000, 50); 
+
+20.
+UPDATE varos SET varos.nepesseg = varos.nepesseg * 1.1 WHERE varos.vnev LIKE "Kuk%"; 
+
+21.
+DELETE FROM varos
+WHERE varos.megyeid=(SELECT megye.id
+FROM megye
+WHERE megye.mnev="Zala");
+
+22.
+SELECT megye.mnev,COUNT(varos.id)
+FROM megye
+LEFT JOIN varos
+ON megye.id=varos.megyeid
+GROUP BY megye.mnev;
+
+20 találat
+
+23.
+CREATE TABLE hajdunevek AS
+SELECT varos.vnev
+FROM varos
+WHERE varos.vnev LIKE "%Hajdú%"
+
+Találat: 6
+24.
+
+CREATE VIEW nagyvarosok AS
+SELECT varos.id, varos.vnev, varos.nepesseg, varos.terulet, megye.mnev, varostipus.vtip
+FROM varos
+INNER JOIN varostipus
+ON varostipus.id=varos.vtipid
+INNER JOIN megye
+ON megye.id=varos.megyeid
+WHERE varos.nepesseg > 50000
+
+Találat: 18
+
+25.
+START TRANSACTION;
+CREATE TABLE seged AS
+SELECT varos.nepesseg,varos.vnev
+FROM varos
+INNER JOIN megye
+ON megye.id = varos.megyeid
+WHERE megye.mnev = "Hajdú-Bihar";
+UPDATE seged SET seged.nepesseg = seged.nepesseg*1.1
+WHERE seged.nepesseg>50000;
+UPDATE seged SET seged.nepesseg = seged.nepesseg*0.9
+WHERE seged.nepesseg<=50000;
+COMMIT;
+
