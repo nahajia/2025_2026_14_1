5b6d50a68f6f542a56cf9f49bf2055ba2c9bff93
Aniko <nahajia@gmail.com>
2025-09-16 09:03:10 +0200

    adatb termek2025

---
+++ 05_termek/127_0_0_1.sql
@@ -0,0 +1,146 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2025. Sze 16. 09:02
+-- Kiszolgáló verziója: 10.4.28-MariaDB
+-- PHP verzió: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `termek2025`
+--
+CREATE DATABASE IF NOT EXISTS `termek2025` DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
+USE `termek2025`;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `kamu`
+--
+
+CREATE TABLE `kamu` (
+  `id` int(11) NOT NULL,
+  `nev` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `termek`
+--
+
+CREATE TABLE `termek` (
+  `termek_id` int(11) NOT NULL,
+  `termek_nev` varchar(255) NOT NULL,
+  `termek_ar` int(11) NOT NULL,
+  `termek_tipus` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `termek`
+--
+
+INSERT INTO `termek` (`termek_id`, `termek_nev`, `termek_ar`, `termek_tipus`) VALUES
+(1, 'kifli', 85, 1),
+(2, 'zsemle', 75, 1),
+(3, 'Túró Rudi', 250, 2),
+(4, 'Vodka', 3500, 3),
+(5, 'Riska tej', 300, 2),
+(6, 'ivójoghurt Barackos', 800, 2),
+(10, 'teszt', 100, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `tipus`
+--
+
+CREATE TABLE `tipus` (
+  `tipus_id` int(11) NOT NULL,
+  `tipus_nev` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `tipus`
+--
+
+INSERT INTO `tipus` (`tipus_id`, `tipus_nev`) VALUES
+(1, 'pékáru'),
+(2, 'tejtermék'),
+(3, 'alkohol'),
+(4, 'gyömülcs'),
+(5, 'zöldség'),
+(6, 'tisztítószer'),
+(11, 'üdítők'),
+(13, 'húsipari termékek');
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `kamu`
+--
+ALTER TABLE `kamu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `termek`
+--
+ALTER TABLE `termek`
+  ADD PRIMARY KEY (`termek_id`),
+  ADD KEY `termek_tipus` (`termek_tipus`);
+
+--
+-- A tábla indexei `tipus`
+--
+ALTER TABLE `tipus`
+  ADD PRIMARY KEY (`tipus_id`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `kamu`
+--
+ALTER TABLE `kamu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT a táblához `termek`
+--
+ALTER TABLE `termek`
+  MODIFY `termek_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT a táblához `tipus`
+--
+ALTER TABLE `tipus`
+  MODIFY `tipus_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `termek`
+--
+ALTER TABLE `termek`
+  ADD CONSTRAINT `termek_ibfk_1` FOREIGN KEY (`termek_tipus`) REFERENCES `tipus` (`tipus_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
