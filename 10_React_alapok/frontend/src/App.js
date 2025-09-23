f5e57829494ee6e8ae4dabff1d2eee622685a7d0
Aniko <nahajia@gmail.com>
2025-09-23 13:58:40 +0200

    react alapok

---
+++ 10_React_alapok/frontend/src/App.js
@@ -0,0 +1,63 @@
+import kep from "./kep.jpg"
+import baross from "./baross.webp"
+import './App.css';
+import Uzenet from "./Uzenet";
+
+const szemely={
+  "nev":"Elgá Zoltán",
+  "iskola":"Baross"
+}
+
+function Nevjegy(){
+    return (
+      <div>
+        <p>Készítette: {szemely.nev}</p>
+        <p>{szemely.iskola}</p>
+        <img src={baross} alt="Baross" />
+      </div>
+    )
+}
+
+function Utazas(){
+  return (
+    <div className="keretUtazas">
+      <p>Utazz el például ide:</p>
+      <ul>
+        <li>Olaszország</li>
+        <li>Spanyolország</li>
+        <li>Görögország</li>
+      </ul>
+    </div>
+  )
+}
+
+const Konyv=()=>{
+  return(
+    <div className="keretUtazas keretZold">
+      <p>Olvass könyveket, például ezeket:</p>
+      <ul>
+        <li>Harry Potter</li>
+        <li>Gyűrűk ura</li>
+        <li>Dan Brown könyvek</li>
+      </ul>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Élj boldogan!</h1>
+      <h2>Pozitív gondolatok a boldog élethez!!!</h2>
+      <img src={kep} alt="Smiley" className="kepkinezet"/>
+
+      <Uzenet />
+      <Konyv />
+      <Utazas />
+      <Nevjegy />
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ 10_React_alapok/frontend/src/Uzenet.js
@@ -0,0 +1,42 @@
+import './App.css';
+const Uzenet=()=>{
+    return (
+        <div className="keretUzenet">
+            <p style={{fontWeight:'bold', fontFamily:"serif", fontSize:20}}>Hasznos tanácsok:</p>
+            <ul>
+                <li>Dolgozz olyat, amit szeretsz!</li>
+                <li>Utazz sokat!</li>
+                <li>Sportolj!</li>
+                <li>Táplálkozz egészségesen!</li>
+                <li>Légy sokat szabad levegőn!</li>
+                <li>Nevess sokat!</li>
+            </ul>
+
+        </div>
+    )
+}
+export default Uzenet
+
+
+/*
+export default function Uzenet(){
+    return (
+        <div>
+            <p>Hasznos tanácsok:</p>
+        </div>
+    )
+}
+*/
+/*
+function Uzenet(){
+    return (
+        <div>
+            <p>Hasznos tanácsok:</p>
+        </div>
+    )
+}
+
+export default Uzenet
+*/
+
+
