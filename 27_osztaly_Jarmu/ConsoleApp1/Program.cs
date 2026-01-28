5cb713c06771115a1109dd5c1b855d28a6f66986
Aniko <nahajia@gmail.com>
2026-01-28 12:37:17 +0100

    Jarmu abstract

---
+++ 27_osztaly_Jarmu/ConsoleApp1/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class Jarmu
+    {
+        private int uzemanyag;//csak ebben az osztályban érhető el
+        protected int Sebesseg { get; private set; } //ebben az osztályban és a származtatott osztályban érhető el
+        public string Rendszam { get; }
+
+        protected Jarmu(string rendszam, int kezdoUzemanyag)
+        {
+            Rendszam = rendszam;
+            uzemanyag = kezdoUzemanyag;
+            Sebesseg = 0;
+        }
+
+        public void Tankol(int liter)
+        {
+            if (liter <= 0)
+            {
+                Console.WriteLine("A tankolt mennyiség pozitív kell legyen. Tankolás elmaradt.");
+                return; // kilép a metódusból
+            }
+
+            uzemanyag += liter;
+        }
+
+        public int UzemanyagSzint()
+        {
+            return uzemanyag;
+        }
+
+
+        protected void SebessegNovel(int ertek)
+        {
+            if (ertek <= 0)
+                return;
+
+            Sebesseg += ertek;
+            uzemanyag -= ertek / 2; // fogyasztás szimulálása
+        }
+
+
+        public abstract void Gyorsit();//az ebből származtatott osztályban kötelező felülírni
+
+        public virtual void KiirAllapot()//van alapértelmezett implementáció, de a leszármazott tetszőlegesen felülírhatja(override).
+        {
+            Console.WriteLine($"Rendszám: {Rendszam}");
+            Console.WriteLine($"Sebesség: {Sebesseg} km/h");
+            Console.WriteLine($"Üzemanyag: {uzemanyag} liter");
+        }
+    }
+    class Auto : Jarmu
+    {
+        public Auto(string rendszam)
+            : base(rendszam, 50)
+        {
+        }
+
+        public override void Gyorsit()
+        {
+            SebessegNovel(10);
+        }
+
+        public override void KiirAllapot()
+        {
+            base.KiirAllapot();
+            Console.WriteLine("Típus: Autó");
+        }
+    }
+    
+    class Motor : Jarmu
+    {
+        public Motor(string rendszam) : base(rendszam, 20){
+
+        }
+        public override void Gyorsit()
+        {
+            SebessegNovel(20);
+        }
+        public override void KiirAllapot()
+        {
+            base.KiirAllapot();
+            Console.WriteLine("Ez a jármű MOTOR!!!");
+        }
+
+    }
+    
+
+    class Program
+    {
+        static void Main()
+        {
+
+            Auto ujAuto1 = new Auto("ABC-123");
+            ujAuto1.KiirAllapot();
+            ujAuto1.Gyorsit();
+            ujAuto1.KiirAllapot();
+
+            Motor ujMotor1 = new Motor("XYZ-111");
+            ujMotor1.KiirAllapot();
+            ujMotor1.Gyorsit();
+            ujMotor1.KiirAllapot();
+            ujMotor1.Tankol(20);
+            ujMotor1.KiirAllapot();
+
+
+            Console.ReadKey();
+        }
+    }
+
+}
+
