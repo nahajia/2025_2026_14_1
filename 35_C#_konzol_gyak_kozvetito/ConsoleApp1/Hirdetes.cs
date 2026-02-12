e5b465121bd6a875114b17eb59ae0d0250513cd2
Aniko <nahajia@gmail.com>
2026-02-12 08:12:31 +0100

    C# konzol kozvetito nemteljes

---
+++ 35_C#_konzol_gyak_kozvetito/ConsoleApp1/Hirdetes.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Hirdetes
+    {
+
+        //SELECT `id`, `ingatlanid`, `ar`, `allapot`, `datum` FROM `hirdetes` WHERE 1
+        public int id { get; set; }
+        public int ingatlanid { get; set; }
+        public double ar { get; set; }
+        public string allapot { get; set; }
+        public string datum { get; set; }
+
+
+        public string Kategoria()
+        {
+            if (this.ar>100)
+            {
+                return "drága";
+            }
+            else if (this.ar>10)
+            {
+                return "közepes";
+            }
+            return "olcsó";
+        }
+        public string Datumformatum
+        {
+            get
+            {
+                string[] kecske = this.datum.Split('T');
+                return kecske[0];
+            }
+        }
+        public string HonapNeve
+        {
+            get
+            {
+                string[] kecske = this.datum.Split('T');
+                string[] szocske = kecske[0].Split('-');
+                int honap = int.Parse(szocske[1]);
+                switch (honap)
+                {
+                    case 1:
+                        return "Január";
+                    case 2:
+                        return "Február";
+                    case 3:
+                        return "Március";
+                    case 4:
+                        return "Április";
+                    case 5:
+                        return "Május";
+                    case 6:
+                        return "Június";
+                    case 7:
+                        return "Július";
+                    case 8:
+                        return "Augusztus";
+                    case 9:
+                        return "Szeptember";
+                    case 10:
+                        return "Október";
+                    case 11:
+                        return "November";
+                    case 12:
+                        return "December";
+                    default:
+                        return "";
+                }
+                
+            }
+        }
+    }
+}
---
+++ 35_C#_konzol_gyak_kozvetito/ConsoleApp1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkHelper;
+using ConsoleTableExt;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Hirdetes> tomb = Backend.GET("http://localhost:3000/hirdetes").Send().As<List<Hirdetes>>();
+            //6. Az eladott ingatlanok kilistázása, ár, dátum
+            var eladott = tomb.Where(x => x.allapot == "eladva")
+                .ToList();
+            foreach (var item in eladott)
+            {
+                Console.WriteLine($"{item.ar} {item.datum}");
+            }
+            //7. Az eladott ingatlanok darabszáma
+            var eladottingatlan = tomb.Count(x => x.allapot == "eladva");
+            Console.WriteLine($"Eladott ingatlanok száma:{eladottingatlan}");
+            var ingatlanok = tomb.GroupBy(x => x.allapot).Select(x => new
+            {
+              Állapot_neve=x.Key,
+              Darabszám=x.Count()
+            })
+                .OrderByDescending(x=>x.Darabszám)
+                .ToList();
+            ConsoleTableBuilder.From(ingatlanok).ExportAndWriteLine();
+            //9. Ár bekérése, attól olcsóbbak kiírása, ha nincs olyan, írja ki: Nincs találat
+            try
+            {
+                Console.Write("Add meg árt:");
+                int megadottar = int.Parse(Console.ReadLine());
+                var olcsobbtomb = tomb.Where(x => x.ar < megadottar).ToList();
+                if (!olcsobbtomb.Any())
+                {
+                    Console.WriteLine("Nincs Adat!");
+                }
+                else
+                {
+                    ConsoleTableBuilder.From(olcsobbtomb).ExportAndWriteLine();
+                   
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hiba ijjjnye valamit elirtál "+ex.Message);
+            }
+            //10. Drága ingatlanok fájlba írása: ár, állapot, dátum
+            var fajlbairas = tomb.Where(x => x.ar > 100).Select(x=>$"{x.ar},{x.allapot},{x.Datumformatum}");
+            List<string> lista = new List<string>();
+            File.WriteAllText("Draga.txt", fajlbairas);
+
+           
+            Console.ReadKey();
+
+        }
+
+       
+    }
+}
