02c3486c7251c4bb1575c202d1c8ec42b48271bd
Aniko <nahajia@gmail.com>
2026-02-20 09:32:49 +0100

    backend_alap

---
+++ backend_alap.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const mysql = require('mysql')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+app.use("/kepek",express.static("kepek"))
+app.use("/kepek2",express.static("kepek2"))
+
+const pool = mysql.createPool({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'marvel_2025_login'
+        })
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.get('/film', (req, res) => {
+        const sql=`
+                select *
+                from film
+                `
+        pool.query(sql, (err, result) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).json({error:"Hiba"})
+        }
+        if (result.length===0){
+            return res.status(404).json({error:"Nincs adat"})
+        }
+
+        return res.status(200).json(result)
+        })
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
