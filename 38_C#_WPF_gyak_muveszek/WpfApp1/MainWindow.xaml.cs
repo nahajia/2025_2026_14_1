6af87aac909dad9d62f8c6f0bb0b8e937d97fcc6
Aniko <nahajia@gmail.com>
2026-02-24 11:20:55 +0100

    WPF művészek

---
+++ 38_C#_WPF_gyak_muveszek/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NetworkHelper;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Szemely> tomb = Backend.GET("http://localhost:3000/szemely").Send().As<List<Szemely>>();
+            //MessageBox.Show($"{tomb[0].nev}");
+            cb_muvesz.ItemsSource = tomb;
+            cb_muvesz.DisplayMemberPath = "az"+"nev";
+            cb_muvesz.SelectedIndex = 0;
+
+        }
+
+        private void Cb_muvesz_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var valaszt = cb_muvesz.SelectedItem as Szemely;
+
+            lb_nev.Content = valaszt.nev;
+            lb_ev.Content = valaszt.ev;
+            lb_elozo.Content = valaszt.elozo;
+        }
+    }
+}
---
+++ 38_C#_WPF_gyak_muveszek/WpfApp1/Szemely.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Szemely
+    {
+        public int az { get; set; }
+        public string nev { get; set; }
+        public int ev { get; set; }
+        public string elozo { get; set; }
+       
+    }
+}
