be21cd517ba3f95927e8dd739b64b009ba9bec98
Aniko <nahajia@gmail.com>
2026-03-03 08:35:15 +0100

    wpf euroskills

---
+++ 40_C#_WPF_Euroskills_2/backend/backend.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const mysql = require('mysql')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+app.use("/kepek",express.static("kepek"))
+app.use("/kepek2",express.static("kepek2"))
+
+const pool = mysql.createPool({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'euroskills2026'
+        })
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/versenyzo', (req, res) => {
+        const sql=`
+                select *
+                from versenyzo
+                `
+        pool.query(sql, (err, result) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).json({error:"Hiba"})
+        }
+        if (result.length===0){
+            return res.status(404).json({error:"Nincs adat"})
+        }
+
+        return res.status(200).json(result)
+        })
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
